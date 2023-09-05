8a4fa127449493896b174476b2c6dc009f8592ec
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-09-05 11:58:50 +0200

    add profile picture, name and job to the main card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "@/components/layout";
 import Card from "@/components/card";
 
@@ -5,7 +6,20 @@ export default function Home() {
   return (
     <Layout>
       <Card className="my-4">
-        <p className="font-bold text-xl text-center">Hello World!</p>
+        <div className="flex flex-row justify-between">
+          <Image 
+            className="block rounded"
+            src="/images/profile.jpg" 
+            alt="Profile picture" 
+            width={300}
+            height={300}
+          />
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="font-bold text-4xl">Erwan Saclier</h1>
+            <div className="h-[1px] bg-gray-400 w-40"></div>
+            <p>Alternance Développeur Full Stack</p>
+          </div>
+        </div>
       </Card>
     </Layout>
   );
