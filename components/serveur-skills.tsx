a5bb37dd48bc63d5bb03e2f692eb157d11b34267
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-10-30 14:06:27 +0100

    Add apache2 to server tools section

--- components/serveur-skills.tsx
+++ components/serveur-skills.tsx
@@ -2,6 +2,10 @@ import SkillRowSection from "./skills-row-section";
 import FrameworksSkillSection, { SkillRowSectionInterface } from "./skills-row-section";
 
 const serveursSkills: SkillRowSectionInterface[] = [
+  {
+    name: "Apache",
+    url: "/images/techs/apache.png",
+  },
 ]
 
 export default function ServeurSkillsSection() {
