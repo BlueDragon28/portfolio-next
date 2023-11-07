dd486cea8200da3d7d0df0823d53f4ccf9c22edd
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-11-07 14:30:56 +0100

    Update introduction text

--- components/introduction-text.tsx
+++ components/introduction-text.tsx
@@ -7,7 +7,7 @@ export default function IntroductionText(
         Passionné de programmation et de tout ce qui touche à l&apos;informatique/électronique que j&apos;ai commencé à apprendre à l&apos;âge de 13 ans, j&apos;ai décidé d&apos;en faire mon métier. Étant curieux de nature, j&apos;aime expérimenter et découvrir de nouvelles technologies.
       </p>
       <p className="my-3 text-justify">
-        J&apos;ai réalisé de nombreux projets dans de nombreux domaines (programmation, réseaux, machines virtuelles...), je possède donc une connaissance approfondie de la programmation et sais m&apos;adapter rapidement à de nouveaux environnements.
+        J&apos;ai réalisé de nombreux projets dans de nombreux domaines (programmation, réseaux, machines virtuelles...), je possède donc une connaissance approfondie sur un certain nombre de sujets et sais m&apos;adapter rapidement à de nouveaux environnements.
       </p>
     </div>
   );
