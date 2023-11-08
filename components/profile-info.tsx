319d2442c52c4439414cc6c73de53d09735456e3
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-11-08 14:48:21 +0100

    add link to CV file

--- components/profile-info.tsx
+++ components/profile-info.tsx
@@ -13,7 +13,7 @@ export default function ProfileInfo() {
         <div className="h-[1px] bg-gray-400 w-40 my-1"></div>
         <p className="my-1">Alternance Technicien Informatique</p>
         <div className="flex flex-row justify-around w-[300px] my-1">
-          <OutlinedLink className="hover:text-gray-100" href="#">Obtenir CV</OutlinedLink> 
+          <OutlinedLink className="hover:text-gray-100" href="CV_ErwanSaclier.pdf">Obtenir CV</OutlinedLink> 
           <OutlinedLink className="hover:text-gray-100" href="/contact">Contact</OutlinedLink>
         </div>
       </div>
